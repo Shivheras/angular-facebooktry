408330d9a81a992a2ca94800a309abc408fcf9af
Shivheras <shivheras@gmail.com>
2020-01-27 11:30:03 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { Component ,OnInit} from '@angular/core';
 import {User} from './user.module';
-import { FormBuilder,FormGroup,Validators } from "@angular/forms"
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  implements OnInit {
+export class AppComponent   {
  
-  user:User = new User();
-  userFormGroup:FormGroup;
 
-constructor(private formBuilder:FormBuilder){
 
 }
-}
+
